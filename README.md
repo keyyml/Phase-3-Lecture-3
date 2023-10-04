0c40e2432189142189b235b38ec596fc93ff1f30
Eleanor Kelman <ekelman3@gmail.com>
2023-10-04 10:48:05 -0400

    set up lecture

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Phase-3-Lecture-3
+# Phase 3 Lecture - Class Attributes and Class Methods
+
+## To get started:
+
+0. Shell into correct Python version
+1. Run `pipenv install` to install the dependencies for the project
+2. Run `pipenv shell` to enter into the project's virtual environment/shell
+
+## To run each file:
+
+1. Run `python src/classes/Book.py` or `python3 src/classes/Book.py`.
---
+++ src/classes/Book.py
@@ -0,0 +1,7 @@
+# define a class Book with 4 properties: title, author, page_count
+# define a class attribute all that stores all objects
+    # when should this attribute be initialized? when should it be used?
+# define 3 class methods: get_all_books that returns all books,
+#                         get_avg_page_count that returns the mean page count
+#                         get_longest that returns the longest book
+    # what are two ways of defining these class methods?
